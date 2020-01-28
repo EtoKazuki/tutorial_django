76efd3673a0abc80b973e86199cd2397f5609100
EtoKazuki <gosukazuki.lol@gmail.com>
2020-01-28 05:15:24 +0000

    a

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -13,7 +13,7 @@ class IndexView(generic.ListView):
     context_object_name = 'latest_question_list'
 
     def get_queryset(self):
-        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date'))[:5]
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
 
 
 class DetailView(generic.DetailView):
@@ -27,19 +27,19 @@ class ResultsView(generic.DetailView):
 
 class DetailView(generic.DetailView):
     def get_queryset(self):
-        return Question.objects.filter(pub_date_lte=timezone.now())
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     try:
-	       selected_choice = question.choice_set.get(pk=request.POST['choice'])
-    except (KeyError, Choice.DoesNotExist):
-    	return render(request, 'polls/detail.html', {
-    		'question':question,
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except(KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', {
+                'question':question,
     		'error_message':"You didn't select a choice",
-    	})
+    		})
     else:
-    	selected_choice.votes += 1
-    	selected_choice.save()
-    	return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
